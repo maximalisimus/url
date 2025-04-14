cc31234350f5a1460c421942d4f966f67cd8fa38
maximalis171091@yandex.ru <maximalis171091@yandex.ru>
2025-04-14 11:59:15 +0300

    Update

---
+++ html/next.js
@@ -0,0 +1,23 @@
+var elements = document.querySelectorAll('.content');
+
+function copyToClipboard(str) {
+ var area = document.createElement('textarea');
+ 
+ document.body.appendChild(area);
+ area.value = str;
+ area.select();
+ document.execCommand("copy");
+ document.body.removeChild(area);
+}
+
+for (let elem of elements) {
+	elem.onclick = event => {
+		if (event.target.className === 'selections') {
+			let selli = document.querySelector('p.selections:hover');
+			selli.addEventListener('click', ()=>{
+				copyToClipboard(selli.innerHTML)
+			})
+		}
+	}
+}
+
